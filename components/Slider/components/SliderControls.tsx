57c1dcaee713e1c8151f15f2eb81f665e01356c8
Manuel Silva <manuel.silvag1@gmail.com>
2021-02-11 02:59:24 -0500

    Eliminadas demos de las fuentes (Archivos innecesarios)

--- components/Slider/components/SliderControls.tsx
+++ components/Slider/components/SliderControls.tsx
@@ -37,7 +37,7 @@ const SliderControls : FC<Props> = ({ delay } : Props) => {
     }
   }, [currentPage, totalPages, handleNext, activeTimeout, delay]);
 
-  const handleClick = () => {
+  const handleClick = () : void => {
     clearTimeout(controlTimeout);
     setActiveTimeout(false);
   };
