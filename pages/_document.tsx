46995a216ec2519aa50cf6c1b7d6d77a69b326a2
Manuel Silva <manuel.silvag1@gmail.com>
2021-03-18 17:25:07 -0500

    prueba de og-image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,20 +9,13 @@ class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
+  // Open Graph & Twitter images need a full URL including domain
 
   render() : ReactElement {
     return (
       <Html lang="es">
         <Head>
           <script defer src="/animations/menuResonsive.js" />
-          <meta name="description" content="Marketing & People Management aplicando ejes claves para el desarrollo empresarial como Business Intelligence (BI) y Business Management (BM). El producto de fusionar estos campos fue la formación de la consigna: Por marcas más humanas y con resultados inmediatos de gran impacto" />
-          <meta property="og:image" content="/img/mapeo-sharing.png" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta property="og:title" content="MAPEO: Soluciones a medida para tu empresa" />
-          <meta property="og:description" content="Creamos soluciones que desafían lo tradicional, humanizando marcas que generen resultados de gran impacto" />
-          <meta charSet="utf-8" />
         </Head>
         <body>
           <Main />
